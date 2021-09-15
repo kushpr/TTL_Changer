534fdff5173b5b58407b3b6e51fa3358a90a52e3
kushpr <89697015+kushpr@users.noreply.github.com>
2021-09-15 19:37:38 -0400

    Updated code and added support for S21+

--- service.sh
+++ service.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 
- set_ttl_65()
+set_ttl_65()
 {
   echo 65 > /proc/sys/net/ipv4/ip_default_ttl
   echo 65 > /proc/sys/net/ipv6/conf/all/hop_limit
@@ -10,6 +10,7 @@ MODDIR=${0%/*}
 }
 mark_traffic_ttl()
 {  
+   iptables -t mangle -A PREROUTING -i p2p0 -j TTL --ttl-set 65
    iptables -t mangle -A PREROUTING -i v4-rmnet0 -j TTL --ttl-set 65
    iptables -t mangle -A PREROUTING -i rmnet0 -j TTL --ttl-set 65
    iptables -t mangle -A PREROUTING -i rmnet1 -j TTL --ttl-set 65
@@ -22,21 +23,26 @@ mark_traffic_ttl()
 }
 route_hotspottraffic_tovpn()
 {
- iptables -t nat -I POSTROUTING 1 -o tun0 -j MASQUERADE
- iptables -I FORWARD 1 -i tun0 -o swlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT 
- iptables -I FORWARD 1 -i swlan0 -o tun0 -j ACCEPT
+   iptables -t nat -I POSTROUTING 1 -o tun0 -j MASQUERADE
+   iptables -I FORWARD 1 -i tun0 -o swlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT 
+   iptables -I FORWARD 1 -i swlan0 -o tun0 -j ACCEPT
 }
 settings put global tether_dun_required 0
 
 if [ -x "$(command -v iptables)" ]
 then
-	if grep -q TTL /proc/net/ip_tables_targets
+	if [ `grep -q TTL /proc/net/ip_tables_targets` ]
 	then
-		iptables -t mangle -A PREROUTING -i rmnet0 -j TTL --ttl-set 65
-        iptables -t mangle -A PREROUTING -i rmnet1 -j TTL --ttl-set 65
-        iptables -t mangle -A PREROUTING -i rmnet_data0 -j TTL --ttl-set 65
-        iptables -t mangle -A PREROUTING -i swlan0 -j TTL --ttl-set 65
-        iptables -t mangle -A PREROUTING -i tun0 -j TTL --ttl-set 65
+         iptables -t mangle -A PREROUTING -i p2p0 -j TTL --ttl-set 65
+         iptables -t mangle -A PREROUTING -i v4-rmnet0 -j TTL --ttl-set 65
+         iptables -t mangle -A PREROUTING -i rmnet0 -j TTL --ttl-set 65
+         iptables -t mangle -A PREROUTING -i rmnet1 -j TTL --ttl-set 65
+         iptables -t mangle -A PREROUTING -i ap0 -j TTL --ttl-set 65
+         iptables -t mangle -A PREROUTING -i swlan0 -j TTL --ttl-set 65
+         iptables -t mangle -A PREROUTING -i tun0 -j TTL --ttl-set 65
+         iptables -t mangle -A PREROUTING -i ipv6_vti0 -j TTL --ttl-set 65
+         iptables -t mangle -A PREROUTING -i rmnet_data0 -j TTL --ttl-set 65
+         iptables -t mangle -A PREROUTING -i rmnet_data1 -j TTL --ttl-set 65
 	else
 		set_ttl_65
 		mark_traffic_ttl
